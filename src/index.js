6531fc938ef46b99df02582809d0ebfa60db25c6
lashon74 <lashonhud@gmail.com>
2020-10-26 08:16:20 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// Extends array to the next you want to add it to can add it at any point
+// const citrus = ["Lime", "Lemon", "Orange"];
+// const fruit = ["Apple", "Banana", "Coconut", ...citrus];
+
+// const fullName = {
+//   fName: "James",
+//   lName: "Bond"
+// };
+
+// const user = {
+//   ...fullName,
+//   id: 1,
+//   username: "jamesbond007"
+// };
+
+// console.log(user);
